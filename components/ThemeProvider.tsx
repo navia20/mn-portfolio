e6951a922d5476ef4757ef334c999a7d7677d559
Matias Navia <maty.naviaaa@gmail.com>
2026-07-22 04:32:52 -0400

    fix: set dark as default theme

--- components/ThemeProvider.tsx
+++ components/ThemeProvider.tsx
@@ -8,7 +8,7 @@ export function ThemeProvider({
   children: React.ReactNode;
 }) {
   return (
-    <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
+    <NextThemesProvider attribute="class" defaultTheme="dark" enableSystem>
       {children}
     </NextThemesProvider>
   );
